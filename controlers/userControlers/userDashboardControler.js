2e58b0b0a08e1fac51522611d679a9b9edcad8c4
nerdflaw <142474238+nerdflaw@users.noreply.github.com>
2024-05-27 16:01:59 +0530

    fix : use puppeteer-core error

--- controlers/userControlers/userDashboardControler.js
+++ controlers/userControlers/userDashboardControler.js
@@ -4236,7 +4236,7 @@ const userGenerateInvoice_get = async (req, res )=> {
 	const executablePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe';
 	console.log(executablePath , 'executablePath')
 	const browser = await puppeteer.launch({
-		headless : false,
+		headless : 'new',
 		executablePath : '/usr/bin/chromium-browser'
 	});
 	const page = await browser.newPage();
